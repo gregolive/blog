58404fe3387b7050a2467edecd25cdb448154bb3
Gregory Olive <gregoryolive@outlook.com>
2021-11-09 01:28:29 +0900

    Fixed issue with posting comments or new articles, added ability to insert newline in article body.

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -3,17 +3,21 @@ module Visible
 
   VALID_STATUSES = ['public', 'private', 'archived']
 
+=begin
   included do
     validates :status, inclusion: { in: VALID_STATUSES }
   end
+=end
 
   class_methods do
     def public_count
       where(status: 'public').count
     end
   end
-
+  
+=begin
   def archived?
     status == 'archived'
   end
+=end
 end
