d38614ff5067d1b208eb2eb47d5ad0c68260b216
Gregory Olive <gregoryolive@outlook.com>
2021-11-10 21:52:22 +0900

    Added media queries for index list grid and tweaked formatting.

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -3,21 +3,17 @@ module Visible
 
   VALID_STATUSES = ['public', 'private', 'archived']
 
-=begin
   included do
     validates :status, inclusion: { in: VALID_STATUSES }
   end
-=end
 
   class_methods do
     def public_count
       where(status: 'public').count
     end
   end
-  
-=begin
+
   def archived?
     status == 'archived'
   end
-=end
 end
